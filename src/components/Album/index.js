404d10ea85c4461c68138b5e66b6c569492d8aad
bagusistighfar86 <bagusistighfar86@gmail.com>
2022-03-28 16:24:56 +0700

    Looping Comoponent using map

--- src/components/Album/index.js
+++ src/components/Album/index.js
@@ -4,17 +4,17 @@ const Album = ({ data }) => {
   useEffect(()=>{
     try{
       console.log(data);
-      const imgAlbum = document.getElementById("img-album");
-      const albumTitle = document.getElementById("album-title");
-      const artistName = document.getElementById("artist-name");
-      const releaseDate = document.getElementById("release-date");
-      const totalTracks = document.getElementById("total-tracks");
+      // const imgAlbum = document.getElementById("img-album");
+      // const albumTitle = document.getElementById("album-title");
+      // const artistName = document.getElementById("artist-name");
+      // const releaseDate = document.getElementById("release-date");
+      // const totalTracks = document.getElementById("total-tracks");
   
-      imgAlbum.src = data.album.images[1].url;
-      albumTitle.innerText = data.album.name;
-      artistName.innerText = data.artists[0].name;
-      releaseDate.innerText = data.album.release_date;
-      totalTracks.innerText = data.album.total_tracks;
+      // imgAlbum.src = data.album.images[1].url;
+      // albumTitle.innerText = data.album.name;
+      // artistName.innerText = data.artists[0].name;
+      // releaseDate.innerText = data.album.release_date;
+      // totalTracks.innerText = data.album.total_tracks;
     }
     catch(err){
       alert(err.message);
@@ -24,16 +24,16 @@ const Album = ({ data }) => {
   return (
     <div className="track-info-album">
         <div className="album-img">
-        <img id='img-album' src="#" />
+          <img id='img-album' src={data.album.images[1].url} />
         </div>
         <div className="album-detail text-white">
-        <h3>Album</h3>
-        <h1 id="album-title"></h1>
-        <div className="artist-info">
-            <p>Artist : <span id="artist-name"></span></p>
-            <p>Release Date : <span id="release-date"></span></p>
-            <p>Total Tracks : <span id="total-tracks"></span></p>
-        </div>
+          <h3>Album</h3>
+          <h1 id="album-title">{data.name}</h1>
+          <div className="artist-info">
+              <p>Artist : <span id="artist-name">{data.album.artists[0].name}</span></p>
+              <p>Release Date : <span id="release-date">{data.album.release_date}</span></p>
+              <p>Total Tracks : <span id="total-tracks">{data.album.total_tracks}</span></p>
+          </div>
         </div>
     </div>
   )
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,12 +6,14 @@ import data from "../../data";
 const Home = () => {
   return (
     <>
-        <Navbar />
-        <div className="main">
-          <div className="container">
-            <Album data = { data }/>
-          </div>
+      <Navbar />
+      <div className="main">
+        <div className="container">
+          {data.map((item) =>(
+            <Album key={item.id} data={item}/>
+          ))}
         </div>
+      </div>
     </>
   );
 };
